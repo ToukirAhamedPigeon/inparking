90adc9d8ed6019799e4e529363d6c56723c89091
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-04-11 20:44:31 +0600

    All

--- components/custom/QRCodeScanner.tsx
+++ components/custom/QRCodeScanner.tsx
@@ -70,7 +70,11 @@ const QrScanner = ({ onScan }: QrScannerProps) => {
   }, [onScan])
 
   return (
-    <div id={qrRegionId} className="w-full bg-slate-300 max-w-md h-64 rounded-xl shadow-xl" />
+    <div
+      id={qrRegionId}
+      className="w-full max-w-md h-64 rounded-xl shadow-xl bg-cover bg-center"
+      style={{ backgroundImage: 'url("/assets/qr_scan_back.jpg")' }}
+    />
   )
 }
 
