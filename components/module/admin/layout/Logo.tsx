7ca8281c416f5e1ba03eee829f4bb7bd5dd4f327
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-04-14 17:23:32 +0600

    লোগো কম্পোনেন্টে গ্যাপ ক্লাসের মান ২ থেকে ৩ এ পরিবর্তন করা হয়েছে।

--- components/module/admin/layout/Logo.tsx
+++ components/module/admin/layout/Logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { cn } from '@/lib/utils';
 export default function Logo({isTitle, className, titleClassName}: {isTitle: boolean, className?: string, titleClassName?: string}){
     return (
-        <div className={cn('flex items-center gap-2', className)}>
+        <div className={cn('flex items-center gap-3', className)}>
             <Image src="/icons/icon-192x192.png" alt="Logo" width={32} height={32}  />
             {isTitle && <span className={cn("text-lg font-semibold", titleClassName)}>In-Parking</span>}
         </div>
