7abc6416b4ab6e060cfcced42f1bf06e545a88cb
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-04-20 22:49:50 +0600

    RouteMapButtons কম্পোনেন্টে বোতামের টেক্সট সাইজ ১৮px থেকে ২০px এ পরিবর্তন করা হয়েছে, যা UI এর পাঠযোগ্যতা উন্নত করবে।

--- components/module/public/homepage/RouteMapButtons.tsx
+++ components/module/public/homepage/RouteMapButtons.tsx
@@ -7,7 +7,7 @@ export default function RouteMapButtons() {
     <div className="w-full flex flex-col lg:flex-row gap-4 mt-6">
       {/* View Animated Route Button */}
       <Button
-        className="w-full lg:w-1/2 px-6 py-10 text-lg flex items-center justify-between gap-3 bg-gradient-to-r from-[#F2C94C] to-[#F2994A]  hover:from-[#F2C94C] hover:to-[#F2994A] rounded-2xl shadow-lg text-white font-semibold text-[18px]"
+        className="w-full lg:w-1/2 px-6 py-10 text-lg flex items-center justify-between gap-3 bg-gradient-to-r from-[#F2C94C] to-[#F2994A]  hover:from-[#F2C94C] hover:to-[#F2994A] rounded-2xl shadow-lg text-white font-semibold text-[20px]"
       >
         <img
           src="/assets/pin.gif"
@@ -19,7 +19,7 @@ export default function RouteMapButtons() {
 
       {/* View Map Images Button */}
       <Button
-        className="w-full lg:w-1/2 px-8 py-10 text-lg font-semibold flex items-center justify-between gap-3 bg-gradient-to-r from-blue-500 to-indigo-600 hover:from-blue-600 hover:to-indigo-700 rounded-2xl shadow-lg"
+        className="w-full lg:w-1/2 px-8 py-10 text-lg font-semibold flex items-center justify-between gap-3 bg-gradient-to-r from-blue-500 to-indigo-600 hover:from-blue-600 hover:to-indigo-700 rounded-2xl shadow-lg text-[20px]"
       >
         <ImageIcon height={200} width={200} className="!w-8 !h-8 md:!w-8 md:!h-8" />
         Map Images
